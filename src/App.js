35b06a71e11e7d3a940ecc71c67f90bd3e57a33c
Michael Maricchiolo <maricchioloemme@gmail.com>
2021-04-12 10:23:19 +0200

    config and install react dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+	return (
+		<Router>
+			<Switch>
+				<Route path='/:lang'>
+          <h1>Home page</h1>
+        </Route>
+        <Route path='*'>
+          <h1>404</h1>
+        </Route>
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
