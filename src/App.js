e9bde66c3bfe216ffec9d5b3e3c9b8a09e5a79bf
Michael Maricchiolo <maricchioloemme@gmail.com>
2021-04-12 10:37:42 +0200

    add styled components config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
+// import Test from './components/Test'
+
 function App() {
 	return (
 		<Router>
 			<Switch>
 				<Route path='/:lang'>
           <h1>Home page</h1>
+          {/* <Test /> */}
         </Route>
         <Route path='*'>
           <h1>404</h1>
