8459bcc326071b46f782f1dcf8c16a43af3d985a
Salvatore Viscuso <salvo.visk@gmail.com>
2021-04-14 11:16:59 +0200

    fix: changed the routing
    now the routing is inside home page and can accept dinamic path using
    useRouterMatch
    
    ref #11

--- src/App.js
+++ src/App.js
@@ -6,42 +6,14 @@ import {
 } from 'react-router-dom';
 import Home from './pages/Home';
 
-const langConfig = {
-  uk: 'en-GB',
-  us: 'en-US',
-  it: 'it-IT',
-  fr: 'fr-FR',
-  de: 'de-DE',
-  es: 'es-ES',
-  pt: 'pt-PT',
-  br: 'pt-BR',
-  ru: 'ru-RU',
-  nl: 'nl-NL',
-  pl: 'pl-PL',
-};
-
 function App() {
-  const lang = window.location.pathname.replace('/', '');
-  const defLang = 'uk';
-
-  const checkLang = () => {
-    if (lang === defLang) {
-      const langKeys = Object.keys(langConfig);
-      const langUrl = langKeys.find((key) => key === lang);
-      return langUrl;
-    } else return defLang;
-  };
-
   return (
     <Router>
       <Switch>
         <Route path='/:lang'>
-          <Redirect push to={`/${checkLang()}`} />
           <Home />
         </Route>
-        <Route path='*'>
-          <h1>404</h1>
-        </Route>
+        <Redirect to='/uk' />
       </Switch>
     </Router>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,18 @@
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
+
 function Home() {
-  return <div>Home Page</div>;
+  let { path } = useRouteMatch();
+  return (
+    <>
+      <div>Header</div>
+      <Switch>
+        <Route path={`${path}/*`}>
+          <h1>404</h1>
+        </Route>
+      </Switch>
+      <div>Footer</div>
+    </>
+  );
 }
 
 export default Home;
