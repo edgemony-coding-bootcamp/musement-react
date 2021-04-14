c98c57e5259c629398249447dec485b5857f42b2
Salvatore Viscuso <salvo.visk@gmail.com>
2021-04-14 11:34:54 +0200

    test: added a link and route
    for the purpose of testing other nested routes after /:lang path

---
+++ src/pages/Activities.js
@@ -0,0 +1,5 @@
+function Activities() {
+  return <div>activities</div>;
+}
+
+export default Activities;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,26 @@
-import { Route, Switch, useRouteMatch } from 'react-router-dom';
+import { Link, Route, Switch, useRouteMatch } from 'react-router-dom';
+// import Activities from './Activities';
 
 function Home() {
-  let { path } = useRouteMatch();
+  let { path, url } = useRouteMatch();
   return (
     <>
       <div>Header</div>
+
+      {/* test link in order to see if the future implementation of nested router with links could be valid
+
+      <Link to={`${url}/activities`}>link ad activities</Link>      
+      
+      */}
+
       <Switch>
+        {/* Test Route
+        
+        <Route exact path={`${path}/activities`}>
+          <Activities />
+        </Route>
+        
+        */}
         <Route path={`${path}/*`}>
           <h1>404</h1>
         </Route>
