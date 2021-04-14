f2e94a772b1f477e42b46ed977415d9f42acc77e
Salvatore Viscuso <salvo.visk@gmail.com>
2021-04-14 12:35:10 +0200

    fix: improved the redirect method to 'uk'
    with the check on a list of supported languages.
    
    ref#11

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,55 @@
-import { Link, Route, Switch, useRouteMatch } from 'react-router-dom';
-// import Activities from './Activities';
+import {
+  Link,
+  Route,
+  Switch,
+  useParams,
+  useHistory,
+  useRouteMatch,
+} from 'react-router-dom';
+import Activities from './Activities';
+
+const langConfig = [
+  'uk',
+  'us',
+  'it',
+  'fr',
+  'de',
+  'es',
+  'pt',
+  'br',
+  'ru',
+  'nl',
+  'pl',
+];
 
 function Home() {
   let { path, url } = useRouteMatch();
+  let { lang } = useParams();
+  let history = useHistory();
+
+  /* Here we are checking the lang param in order to see if it is a supported language,
+  if not, the application will set the default languange in 'uk'  */
+
+  const langUrl = langConfig.find((value) => value === lang);
+  if (!langUrl) {
+    /*  at this point we are creating a dinamic path redirect in order to set the default 
+    languange also in other routes  */
+
+    const newUrl = history.location.pathname.replace(`/${lang}`, '');
+    history.push(`/uk${newUrl}`);
+  }
   return (
     <>
       <div>Header</div>
 
-      {/* test link in order to see if the future implementation of nested router with links could be valid
-
-      <Link to={`${url}/activities`}>link ad activities</Link>      
-      
-      */}
+      {/* this link and the activities route are for test purposes */}
+      <Link to={`${url}/activities`}>link ad activities</Link>
 
       <Switch>
-        {/* Test Route
-        
         <Route exact path={`${path}/activities`}>
           <Activities />
         </Route>
-        
-        */}
+
         <Route path={`${path}/*`}>
           <h1>404</h1>
         </Route>
