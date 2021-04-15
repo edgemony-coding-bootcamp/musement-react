4dead3846c880217191d0f30036b94ef48739e38
Alfredo <alfredomeschis@yahoo.com>
2021-04-15 22:36:53 +0200

    fix: resolve conflicts in App and Home Components.
    ref #3

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Home from './pages/Home';
 import Header from './components/Header';
 import './App.css';
 
+import { DEF_LANG } from './config.json';
+
 const langConfig = {
   uk: 'en-GB',
   us: 'en-US',
@@ -35,16 +37,25 @@ function App() {
   };
 
   return (
+    // <Router>
+    //   <Header />
+    //   <Switch>
+    //     <Route path='/:lang'>
+    //       <Redirect push to={`/${checkLang()}`} />
+    //       <Home />
+    //     </Route>
+    //     <Route path='*'>
+    //       <h1>404</h1>
+    //     </Route>
+    //   </Switch>
+    // </Router>
     <Router>
       <Header />
       <Switch>
         <Route path='/:lang'>
-          <Redirect push to={`/${checkLang()}`} />
           <Home />
         </Route>
-        <Route path='*'>
-          <h1>404</h1>
-        </Route>
+        <Redirect to={`/${DEF_LANG}`} />
       </Switch>
     </Router>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,53 @@
 import Hero from '../components/Hero';
 import CategoriesNav from '../components/CategoriesNav';
 import { Main } from '../styles';
+import {
+  Route,
+  Switch,
+  useParams,
+  useHistory,
+  useRouteMatch,
+} from 'react-router-dom';
+
+import { DEF_LANG } from '../config.json';
+
+const langConfig = [
+  'uk',
+  'us',
+  'it',
+  'fr',
+  'de',
+  'es',
+  'pt',
+  'br',
+  'ru',
+  'nl',
+  'pl',
+];
+
 function Home() {
+  let { path } = useRouteMatch();
+  let { lang } = useParams();
+  let history = useHistory();
+
+  const langUrl = langConfig.find((value) => value === lang);
+  if (!langUrl) {
+    const newUrl = history.location.pathname.replace(`/${lang}`, '');
+    history.push(`/${DEF_LANG}${newUrl}`);
+  }
   return (
     <>
-      <CategoriesNav />
-      <Main>
-        <Hero />
-      </Main>
+      <Switch>
+        <Route path={`/${lang}`}>
+          <CategoriesNav />
+          <Main>
+            <Hero />
+          </Main>
+        </Route>
+        <Route path={`${path}/*`}>
+          <h1>404</h1>
+        </Route>
+      </Switch>
     </>
   );
 }
