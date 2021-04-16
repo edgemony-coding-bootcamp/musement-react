1f4a61ce9fc74d6ac758f03b3a5ad2d6c93d856f
Salvatore Viscuso <salvo.visk@gmail.com>
2021-04-16 11:12:31 +0200

    fix: removed console log

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import { useSelector } from 'react-redux';
 
 function App() {
   const { userLang } = useSelector((state) => state.languages);
-  console.log(userLang);
   SetLangInEndpoint(userLang);
 
   return (
