4f34f5ac4000d0f178f2ecd4e65fa07c552c3d99
Salvatore Viscuso <salvo.visk@gmail.com>
2021-04-21 15:57:50 +0200

    feat: dynamic header for currency ref #25

--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -1,11 +1,16 @@
 import axios from 'axios';
-import { BASE_URL, API_VERSION } from '../config.json';
+import { BASE_URL, API_VERSION, DEF_CURR } from '../config.json';
 
 export const setUrlandVersionHeader = () => {
   axios.defaults.baseURL = `${BASE_URL}`;
   axios.defaults.headers['x-musement-version'] = `${API_VERSION}`;
+  axios.defaults.headers['x-musement-currency'] = `${DEF_CURR}`;
 };
 
 export const setLangHeader = (userLang) => {
   axios.defaults.headers['accept-language'] = `${userLang}`;
+};
+
+export const setCurrencyHeader = (userCurr) => {
+  axios.defaults.headers['x-musement-currency'] = `${userCurr}`;
 };
