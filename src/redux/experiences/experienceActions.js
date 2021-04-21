7198b6a8ebedd5c18bf439feb92b44412e196781
Salvatore Viscuso <salvo.visk@gmail.com>
2021-04-21 13:51:56 +0200

    fix: fixed experiences bug  and improved the result of the dispatch

--- src/redux/experiences/experienceActions.js
+++ src/redux/experiences/experienceActions.js
@@ -10,12 +10,17 @@ export const expIds = [88080, 87218, 35703];
 export const fetchExperiences = () => {
   return async (dispatch) => {
     dispatch({ type: FETCH_EXPERIENCES_REQUEST });
+    let totalExperiences = [];
     Promise.all(
       // eslint-disable-next-line array-callback-return
       expIds.map((expId) => {
         try {
           axios.get(`/activities/${expId}`).then((res) => {
-            dispatch({ type: FETCH_EXPERIENCES_SUCCESS, payload: res.data });
+            totalExperiences = [...totalExperiences, res.data];
+            dispatch({
+              type: FETCH_EXPERIENCES_SUCCESS,
+              payload: totalExperiences,
+            });
           });
         } catch (e) {
           dispatch({ type: FETCH_EXPERIENCES_ERROR, payload: e });
