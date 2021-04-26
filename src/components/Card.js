e613c8a8831870a40c094fe9bbb17c38989ccc58
Salvatore Viscuso <salvo.visk@gmail.com>
2021-04-26 11:08:39 +0200

    feat: added link with activity id to card

--- src/components/Card.js
+++ src/components/Card.js
@@ -18,69 +18,79 @@ import {
   CardDivRowFooter,
   CardDivRowDescription,
   CardContentWrapper,
+  LinkPages,
 } from '../styles';
 import { replaceLang, parseISODuration, setPriceFormat } from '../utilities';
 import { ReactComponent as FreeCancellationIcon } from '../assets/img/freeCancellation.svg';
 import { ReactComponent as LanguageIcon } from '../assets/img/languages.svg';
 import { ReactComponent as DurationIcon } from '../assets/img/duration.svg';
+
+import { useRouteMatch } from 'react-router';
 import { useSelector } from 'react-redux';
 
 const Card = ({ content }) => {
+  let { path } = useRouteMatch();
   const { translatedTexts } = useSelector((state) => state.translations);
   return (
     <>
       {content ? (
-        <CardWrapper>
-          <CardImg src={content.cover_image_url} alt='image'></CardImg>
-          <CardContentWrapper>
-            <CardSectionHeader>
-              <CardCategoryLabel>{`${
-                content.ticket
-                  ? `${translatedTexts.ticketandevents}`
-                  : `${translatedTexts.attractionandguidetour}`
-              } `}</CardCategoryLabel>
-              <CardTitle>{content.title}</CardTitle>
-              <CardDescription>{content.description}</CardDescription>
-            </CardSectionHeader>
-            <CardSectionBody>
-              <CardCancellation cancellation={content.free_cancellation}>
-                <IconBodyCard>
-                  <FreeCancellationIcon />
-                </IconBodyCard>
-                {translatedTexts.freecancellation}
-              </CardCancellation>
-              <CardDurationValue>
-                <IconBodyCard>
-                  <DurationIcon />
-                </IconBodyCard>
-                {parseISODuration(content.duration)}
-              </CardDurationValue>
-              {content.languages.length > 0 && (
-                <CardDivRowDescription>
+        <LinkPages to={`${path}/activities/${content.uuid}`}>
+          <CardWrapper>
+            <CardImg src={content.cover_image_url} alt='image'></CardImg>
+            <CardContentWrapper>
+              <CardSectionHeader>
+                <CardCategoryLabel>{`${
+                  content.ticket
+                    ? `${translatedTexts.ticketandevents}`
+                    : `${translatedTexts.attractionandguidetour}`
+                } `}</CardCategoryLabel>
+                <CardTitle>{content.title}</CardTitle>
+                <CardDescription>{content.description}</CardDescription>
+              </CardSectionHeader>
+              <CardSectionBody>
+                <CardCancellation cancellation={content.free_cancellation}>
                   <IconBodyCard>
-                    <LanguageIcon />
+                    <FreeCancellationIcon />
                   </IconBodyCard>
-                  {translatedTexts.availablein}
-                  <CardLanguage>{replaceLang(content.languages)}</CardLanguage>
-                </CardDivRowDescription>
-              )}
-            </CardSectionBody>
-            <CardSectionFooter>
-              <CardDivRowFooter>
-                <CardStarWrapper>
-                  <Rating
-                    value={content.reviews_avg}
-                    // eslint-disable-next-line prettier/prettier
-                    numReviews={content.reviews_number}></Rating>
-                </CardStarWrapper>
-              </CardDivRowFooter>
-              <CardPriceWrapper>
-                <CardSpanPrice>{translatedTexts.from}</CardSpanPrice>
-                {setPriceFormat(content.original_retail_price.formatted_value)}
-              </CardPriceWrapper>
-            </CardSectionFooter>
-          </CardContentWrapper>
-        </CardWrapper>
+                  {translatedTexts.freecancellation}
+                </CardCancellation>
+                <CardDurationValue>
+                  <IconBodyCard>
+                    <DurationIcon />
+                  </IconBodyCard>
+                  {parseISODuration(content.duration)}
+                </CardDurationValue>
+                {content.languages.length > 0 && (
+                  <CardDivRowDescription>
+                    <IconBodyCard>
+                      <LanguageIcon />
+                    </IconBodyCard>
+                    {translatedTexts.availablein}
+                    <CardLanguage>
+                      {replaceLang(content.languages)}
+                    </CardLanguage>
+                  </CardDivRowDescription>
+                )}
+              </CardSectionBody>
+              <CardSectionFooter>
+                <CardDivRowFooter>
+                  <CardStarWrapper>
+                    <Rating
+                      value={content.reviews_avg}
+                      // eslint-disable-next-line prettier/prettier
+                      numReviews={content.reviews_number}></Rating>
+                  </CardStarWrapper>
+                </CardDivRowFooter>
+                <CardPriceWrapper>
+                  <CardSpanPrice>{translatedTexts.from}</CardSpanPrice>
+                  {setPriceFormat(
+                    content.original_retail_price.formatted_value
+                  )}
+                </CardPriceWrapper>
+              </CardSectionFooter>
+            </CardContentWrapper>
+          </CardWrapper>
+        </LinkPages>
       ) : null}
     </>
   );
