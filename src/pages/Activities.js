4a87ecd7e62d1ad954ad3cfd61d8997f27cb654d
Salvatore Viscuso <salvo.visk@gmail.com>
2021-04-26 11:09:59 +0200

    feat: mounted activity page with results from api

--- src/pages/Activities.js
+++ src/pages/Activities.js
@@ -1,13 +1,13 @@
-import React /* { useEffect } */ from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { useParams } from 'react-router';
-// import { getActivityById } from '../redux/activities/activityActions';
+import React, { useEffect } from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router';
+import { getActivityById } from '../redux/activities/activityActions';
 import {
   SectionWrapper,
   Jumbotron,
   BackdropImage,
   OpenMediaButton,
-  ContentSection,
   ContentSectionContainer,
   ContentSectionBody,
   CardCategoryLabel,
@@ -20,58 +20,72 @@ import {
   ContentAbout,
 } from '../styles';
 import { parseISODuration } from '../utilities';
-import { data } from './data';
 
 function Activities() {
-  // let { id } = useParams();
-  // const dispatch = useDispatch();
-  // const { activity, loading } = useSelector((state) => state.activity);
-  // useEffect(() => {
-  //   dispatch(getActivityById(id));
-  // }, [dispatch]);
-  return (
-    <>
-      <SectionWrapper>
-        <Jumbotron>
-          <BackdropImage src={data.city['cover_image_url']} />
-          <OpenMediaButton>Open Media</OpenMediaButton>
-        </Jumbotron>
+  let { id } = useParams();
+  const dispatch = useDispatch();
+  const { activity, loading, error } = useSelector((state) => state.activity);
 
-        <ContentSectionContainer>
-          <ContentSectionHeader>
-            <CardCategoryLabel>
-              {data.ticket
-                ? 'Biglietti ed Eventi'
-                : 'Attrazioni e tour guidati'}
-            </CardCategoryLabel>
-            <ContentTitle>{data.title}</ContentTitle>
-            <ContentUrl>{` Home > ${data.city['name']} > ${data.title}`}</ContentUrl>
-          </ContentSectionHeader>
-          <ContentFeaturesSection>
-            <FeaturesDiv>
-              {data.free_cancellation ? 'Cancellazione Gratuita' : null}
-            </FeaturesDiv>
-            <FeaturesDiv>
-              Languages: {data.languages.map((item) => item.name)}
-            </FeaturesDiv>
-            <FeaturesDiv>{data.operational_days}</FeaturesDiv>
-            <FeaturesDiv>{parseISODuration(data.validity)}</FeaturesDiv>
-            <FeaturesDiv>
-              {data.voucher_access_usage ? 'Mobile voucher accepted' : null}
-            </FeaturesDiv>
-            <FeaturesDiv>{'Safety measures applied'}</FeaturesDiv>
-            <FeaturesDiv>
-              <span>{data.features[0].name}</span>
-              <span>{data.features[1].name}</span>
-            </FeaturesDiv>
-          </ContentFeaturesSection>
-          <ContentSectionBody>
-            <ContentDescription>{data.description}</ContentDescription>
-            <ContentAbout>{data.about}</ContentAbout>
-          </ContentSectionBody>
-        </ContentSectionContainer>
-      </SectionWrapper>
-    </>
+  useEffect(() => {
+    dispatch(getActivityById(id));
+  }, []);
+  let activityKeys = Object.keys(activity);
+  return (
+    <div>
+      {loading ? (
+        <div>Loading</div>
+      ) : error ? (
+        <div>error</div>
+      ) : (
+        activityKeys.length > 0 && (
+          <SectionWrapper>
+            <Jumbotron>
+              <BackdropImage src={activity?.city['cover_image_url']} />
+              <OpenMediaButton>Open Media</OpenMediaButton>
+            </Jumbotron>
+            <ContentSectionContainer>
+              <ContentSectionHeader>
+                <CardCategoryLabel>
+                  {activity?.ticket
+                    ? 'Biglietti ed Eventi'
+                    : 'Attrazioni e tour guidati'}
+                </CardCategoryLabel>
+                <ContentTitle>{activity?.title}</ContentTitle>
+                <ContentUrl>{` Home > ${activity?.city['name']} > ${activity?.title}`}</ContentUrl>
+              </ContentSectionHeader>
+              <ContentFeaturesSection>
+                <FeaturesDiv>
+                  {activity?.free_cancellation
+                    ? 'Cancellazione Gratuita'
+                    : null}
+                </FeaturesDiv>
+                <FeaturesDiv>
+                  Languages: {activity?.languages.map((item) => item.name)}
+                </FeaturesDiv>
+                <FeaturesDiv>{activity?.operational_days}</FeaturesDiv>
+                <FeaturesDiv>
+                  {parseISODuration(activity?.validity)}
+                </FeaturesDiv>
+                <FeaturesDiv>
+                  {activity?.voucher_access_usage === 'MOBILE'
+                    ? 'Mobile voucher accepted'
+                    : null}
+                </FeaturesDiv>
+                <FeaturesDiv>{'Safety measures applied'}</FeaturesDiv>
+                <FeaturesDiv>
+                  <span>{activity?.features[0]?.name}</span>
+                  <span>{activity?.features[1]?.name}</span>
+                </FeaturesDiv>
+              </ContentFeaturesSection>
+              <ContentSectionBody>
+                <ContentDescription>{activity?.description}</ContentDescription>
+                <ContentAbout>{activity?.about}</ContentAbout>
+              </ContentSectionBody>
+            </ContentSectionContainer>
+          </SectionWrapper>
+        )
+      )}
+    </div>
   );
 }
 
